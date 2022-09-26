3c934f40a86dbc51baf3cf7e25ebfe60232b0520
weakiam <107641835+weakiam@users.noreply.github.com>
2022-09-26 22:41:26 +0200

    Add files via upload

---
+++ shit_installer.sh
@@ -0,0 +1,2 @@
+sudo dnf install @base-x i3-gaps @hardware-support sddm firefox alacritty timeshift thunar xfce-polkit nitrogen arandr rofi vim neovim neofetch lxappearance breeze-gtk breeze-icon-theme adwaita-gtk2-theme.x86_64 brightnessctl polybar calibre xarchiver php gimp galculator compton geeqie
+#also, you need to install nvidia drivers. do that on rpm fusion. 
