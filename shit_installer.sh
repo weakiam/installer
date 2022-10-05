4603f3de58ea9ec97bd073c1549bfb6c7e025f99
wiktor <107641835+weakiam@users.noreply.github.com>
2022-10-06 01:17:14 +0200

    Update shit_installer.sh

--- shit_installer.sh
+++ shit_installer.sh
@@ -1,3 +1,8 @@
+mv -t ~/.config alacritty nvim i3 polybar rofi
+mv -t ~/ .bashrc .Xmodmap
+
+
+
 sudo dnf update -y
 
 sudo dnf install @base-x i3-gaps @hardware-support sddm firefox alacritty timeshift thunar xfce-polkit nitrogen arandr rofi vim neovim neofetch lxappearance breeze-gtk breeze-icon-theme adwaita-gtk2-theme.x86_64 brightnessctl polybar calibre xarchiver php gimp galculator compton geeqie -y
