886e5bb23071ab87cc82d515baddcee791bc7b30
wiktor <107641835+weakiam@users.noreply.github.com>
2022-10-06 01:09:56 +0200

    Update shit_installer.sh

--- shit_installer.sh
+++ shit_installer.sh
@@ -1,6 +1,8 @@
+sudo dnf update -y
+
 sudo dnf install @base-x i3-gaps @hardware-support sddm firefox alacritty timeshift thunar xfce-polkit nitrogen arandr rofi vim neovim neofetch lxappearance breeze-gtk breeze-icon-theme adwaita-gtk2-theme.x86_64 brightnessctl polybar calibre xarchiver php gimp galculator compton geeqie -y
 
-sudo dnf update -y
+
 sudo systemctl enable sddm 
 sudo systemctl set-default graphical.target
 reboot
