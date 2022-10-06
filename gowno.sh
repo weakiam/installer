e3854cf6e4739e6eb9e0a51d0ce8857bdeed33ca
wiktor <107641835+weakiam@users.noreply.github.com>
2022-10-06 17:54:36 +0200

    Update gowno.sh

--- gowno.sh
+++ gowno.sh
@@ -13,11 +13,11 @@ sudo dnf install @base-x i3-gaps @hardware-support sddm -y
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
+sudo dnf install akmod-nvidia -y
+
 sudo dnf groupupdate core -y
 
 sudo dnf groupupdate sound-and-video -y
-
-sudo dnf install akmod-nvidia -y
 
 sudo dnf install firefox alacritty timeshift thunar xfce-polkit nitrogen arandr rofi vim neovim neofetch lxappearance breeze-gtk breeze-icon-theme adwaita-gtk2-theme.x86_64 brightnessctl polybar calibre xarchiver php gimp galculator compton geeqie -y
 
@@ -29,6 +29,7 @@ cd
 cd .config/polybar
 sudo chmod a+x launch.sh
 cd
+mkdir Documents Downloads Pictures
 modinfo -F version nvidia
 modinfo -F version nvidia
 modinfo -F version nvidia
