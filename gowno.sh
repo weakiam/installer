b8759b4a050194a5e387239fe24bcd251ac53aa1
wiktor <107641835+weakiam@users.noreply.github.com>
2022-10-06 09:09:05 +0200

    Update gowno.sh

--- gowno.sh
+++ gowno.sh
@@ -9,7 +9,17 @@ mv -t ~/ .bashrc .Xmodmap
 
 sudo dnf update -y
 
-sudo dnf install @base-x i3-gaps @hardware-support sddm firefox alacritty timeshift thunar xfce-polkit nitrogen arandr rofi vim neovim neofetch lxappearance breeze-gtk breeze-icon-theme adwaita-gtk2-theme.x86_64 brightnessctl polybar calibre xarchiver php gimp galculator compton geeqie -y
+sudo dnf install @base-x i3-gaps @hardware-support sddm -y
+
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+sudo dnf groupupdate core -y
+
+sudo dnf groupupdate sound-and-video -y
+
+sudo dnf install akmod-nvidia
+
+sudo dnf install firefox alacritty timeshift thunar xfce-polkit nitrogen arandr rofi vim neovim neofetch lxappearance breeze-gtk breeze-icon-theme adwaita-gtk2-theme.x86_64 brightnessctl polybar calibre xarchiver php gimp galculator compton geeqie -y
 
 
 sudo systemctl enable sddm 
